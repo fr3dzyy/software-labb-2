dd74e15cf755667b8ae4754732639a304410cf93
Gibbem <ozmehak.calle@gmail.com>
2022-11-07 20:54:23 +0100

    Added .env, fixed cloudinary integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from "@cloudinary/react";
+import { fill } from "@cloudinary/url-gen/actions/resize";
 
 function App() {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "dvvauf785",
+    },
+  });
+  const myImage = cld.image("pexels-sharath-g-4418671_bef62x");
+  myImage.resize(fill().width(250).height(250));
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <AdvancedImage cldImg={myImage} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
