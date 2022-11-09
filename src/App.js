f1df0ad2129900c527c9a41c7537a15e2c599797
Fredric Ryding <fredricr91@hotmail.com>
2022-11-09 11:34:42 +0100

    Added react-cookie
    * Also removed web-vitals + some code clean-up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import LogRocket from 'logrocket'
-import { ImageCarousel } from "./components/ImageCarousel";
-import {Header} from "./components/Header";
-import {Footer} from "./components/Footer";
+import styled from 'styled-components'
+import { useCookies } from 'react-cookie'
+
+import { ImageCarousel } from './components/ImageCarousel'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 LogRocket.init('znathy/mango-tree')
 
 function App() {
+  const [cookies, setCookie] = useCookies(['name'])
 
-    return (
-        <div className="App">
-            <Header />
-            <ImageCarousel />
-            <Footer />
-        </div>
-    )
+  function onChange() {
+    let expireDate = new Date()
+    expireDate.setTime(expireDate.getTime() + 1 * 3600 * 1000)
+
+    setCookie('name', 'value', {
+      expires: expireDate,
+      httpOnly: false,
+      path: '/',
+      secure: true,
+    })
+  }
+
+  return (
+    <>
+      <Header />
+      <CookieDiv>
+        {!cookies.name && <button onClick={onChange}>Set Cookie</button>}
+        {cookies.name && <h3>Welcome!</h3>}
+      </CookieDiv>
+      <ImageCarousel />
+      <Footer />
+    </>
+  )
 }
 
 export default App
+
+const CookieDiv = styled.div`
+  margin-top: 15px;
+  text-align: center;
+`
